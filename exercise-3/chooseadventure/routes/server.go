9371e95300071d6267e66e3aca1c45275a3558c5
Evan <eslambert77@gmail.com>
2021-02-09 15:45:06 -0600

    Exercise-3 Finally Mod Working

--- exercise-3/chooseadventure/routes/server.go
+++ exercise-3/chooseadventure/routes/server.go
@@ -1,17 +1,26 @@
 package routes
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type apiHandler struct{}
 
 func (apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
 	if r.URL.Path == "/hello" {
-		http.ServeFile(w, r, "hello.html")
+		http.ServeFile(w, r, "html/hello.html")
 	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Henlo!")
 }
 
 func DefaultMux() *http.ServeMux {
 	serv := http.NewServeMux()
 	serv.Handle("/hello", apiHandler{})
+	serv.HandleFunc("/", hello)
 	return serv
 }
--- exercise-3/main.go
+++ exercise-3/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	_ "routes"
+	"net/http"
+
+	"github.com/eslambert77/Gophercises/exercise-3/chooseadventure/routes"
 )
 
 func main() {
 	//Let's get the hello world function and JSON working
+	serv := routes.DefaultMux()
+	http.ListenAndServe(":8080", serv)
 }
