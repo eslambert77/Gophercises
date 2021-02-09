4979b6ced9b5fcf84769022e2f223dcef0981719
Evan <eslambert77@gmail.com>
2021-02-09 13:01:15 -0600

    Exercise-3 Initial server

---
+++ exercise-3/chooseadventure/routes/server.go
@@ -0,0 +1,17 @@
+package routes
+
+import "net/http"
+
+type apiHandler struct{}
+
+func (apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/hello" {
+		http.ServeFile(w, r, "hello.html")
+	}
+}
+
+func defaultMux() *http.ServeMux {
+	serv := http.NewServeMux()
+	serv.Handle("/hello", apiHandler{})
+	return serv
+}
